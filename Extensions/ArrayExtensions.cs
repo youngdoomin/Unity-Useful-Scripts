7247c9c0aefe7f15d7957b9db42a1ba6dabbae9b
Pedro <pedro@cemware.com>
2023-10-16 10:04:09 +0900

    Add Array Extension Script

---
+++ Extensions/ArrayExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+	public static class ArrayExtensions
+	{
+		public static void ForEach<T>(this T[] array, Action<T> action)
+		{
+			Array.ForEach<T>(array, action);
+		}
+		public static bool Contain<T>(this T[] array, T value)
+		{
+			return Array.IndexOf(array, value) != -1;
+		}
+	}
